d59debdaa05ee813883d8a59a5757b42cf1cbea1
Phillip <phillipfranco55@yahoo.com>
2018-05-15 19:32:46 -0700

    Calculates the area of a circle and the perimeter of a rectangle.

---
+++ area_perimeter.py
@@ -0,0 +1,17 @@
+
+
+# Gives the area of a circle.
+pi = 3.1415
+r = input("Enter Radius: ")
+radius = float(r)
+area = pi * radius ** 2
+print("The area of the circle is", area)
+
+
+# Gives the perimeter of a square.
+b = input("Enter the base length of the rectangle: ")
+h = input("Enter the height of the rectangle: ")
+length = int(b) * 2
+height = int(h) * 2
+perimeter = length + height
+print("The perimeter of the square is", perimeter)
