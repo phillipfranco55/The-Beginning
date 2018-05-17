c8d704c0966114ca1fbc9ad555b251815f88f70d
Phillip <phillipfranco55@yahoo.com>
2018-05-17 12:17:16 -0700

    Hello world with header.

---
+++ hello_world_v2.py
@@ -0,0 +1,9 @@
+# Making Hello World with header.
+print('-' * 50)
+print(" " * 17, "HELLO APP", " " * 17)
+print('-' * 50)
+print()
+
+user_input = input("What is your name? ")
+greeting = 'Nice to meet you ' + user_input
+print(greeting)
