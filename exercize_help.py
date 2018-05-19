2db2c6376f1db6cf05e734b8d3cde5198165985a
Phillip <phillipfranco55@yahoo.com>
2018-05-19 09:19:11 -0700

    An exercise in using help()

---
+++ exercize_help.py
@@ -0,0 +1,28 @@
+
+
+name = 'Phillip'
+print(dir(name))
+
+print()
+
+print(help('word'.find('d')))
+
+print()
+
+
+print(help(''.title()))
+
+print()
+
+# Print the dir of my age in a string.
+age = 42
+print(dir(age))
+
+print()
+
+# Print the help of the .numerator method.
+from fractions import Fraction  # This should be up top, i'm keeping it here to bundle it with the exercise.
+a = Fraction(1,2)
+print(type(a))
+print(a)
+print(help(a.numerator))
